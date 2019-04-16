c36953907fd91d9171e01788bc9341b42f482179
Franck GALLIAT <fgalliat@gmail.com>
2019-04-16 12:58:49 +0200

    STILL working on Heap/Heap register managment

--- xts_memory.cpp
+++ xts_memory.cpp
@@ -25,12 +25,14 @@ void _initHRegister() {
 
 // -- heap reg ----
 
-heapAddr getVar(char* name) {
+// BEWARE w/ array cells
+
+int findVar(char* name) {
     for(int i=0; i < HEAP_REG_ENTRY_NB; i++) {
         int addr = i*HEAP_REG_ENTRY_SIZE;
         // reached end of used heap register
         if ( hregister[addr] == 0x80 ) {
-            return HEAP_NOT_FOUND;
+            return -1;
         }
 
         char entryName[ HEAP_REG_ENTRY_SIZE_name +1 ];
@@ -38,15 +40,58 @@ heapAddr getVar(char* name) {
         memcpy( entryName, &(hregister[addr]), HEAP_REG_ENTRY_SIZE_name );
 
         if ( strcmp(entryName, name) == 0 ) {
-            uint8_t add1 = hregister[ addr + HEAP_REG_ENTRY_SIZE_name ];
-            uint8_t add0 = hregister[ addr + HEAP_REG_ENTRY_SIZE_name +1 ];
-
-            heapAddr found = ( add1 << 8 ) + add0;
-            return found;
+            return i;
         }
 
     }
-    return HEAP_NOT_FOUND;
+    return -1;
 }
+
+// return nb of used entries
+int getHRegisterUse() {
+    for(int i=0; i < HEAP_REG_ENTRY_NB; i++) {
+        int addr = i*HEAP_REG_ENTRY_SIZE;
+        // reached end of used heap register
+        if ( hregister[addr] == 0x80 ) {
+            return i;
+        }
+    }
+    return HEAP_REG_ENTRY_NB-1;
+}
+
+heapAddr getVar(char* name) {
+    int idx = findVar(name);
+    if ( idx < 0 ) {
+        return HEAP_NOT_FOUND;
+    }
+
+    int addr = idx*HEAP_REG_ENTRY_SIZE;
+    uint8_t add1 = hregister[ addr + HEAP_REG_ENTRY_SIZE_name +0 ];
+    uint8_t add0 = hregister[ addr + HEAP_REG_ENTRY_SIZE_name +1 ];
+
+    heapAddr found = ( add1 << 8 ) + add0;
+    return found;
+}
+
+void registerVar(char* name, heapAddr vaddr) {
+  int idx = findVar(name);
+  bool found = idx >= 0;
+  if ( found ) {
+    //   reassignVarAddr
+    int addr = idx*HEAP_REG_ENTRY_SIZE;
+    hregister[ addr + HEAP_REG_ENTRY_SIZE_name +0 ] = vaddr >> 8;
+    hregister[ addr + HEAP_REG_ENTRY_SIZE_name +1 ] = vaddr % 256;
+  } else {
+    //   addEntry @ hregister end
+    idx = getHRegisterUse();
+    // BEWARE : w/ overflow detection !!!!
+    int addr = idx*HEAP_REG_ENTRY_SIZE;
+    memset(hregister, 0x00, HEAP_REG_ENTRY_SIZE_name);
+    memcpy( &hregister[addr], name, max( strlen(name), HEAP_REG_ENTRY_SIZE_name ) );
+    hregister[ addr + HEAP_REG_ENTRY_SIZE_name +0 ] = vaddr >> 8;
+    hregister[ addr + HEAP_REG_ENTRY_SIZE_name +1 ] = vaddr % 256;
+  }
+}
+
 
 
