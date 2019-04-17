3e950b7aa887baaabba82cd98315bb8aab0e2791
fgalliat <fgalliat@gmail.com>
2019-04-17 08:10:31 +0200

    CHANGED Heap type 'num' to 'number'

--- xts_memory.h
+++ xts_memory.h
@@ -20,27 +20,27 @@
 
 
  // Cf arduino
- typedef uint32_t num;
+ typedef uint32_t number;
 
- void assignVar(char* name, num value);
+ void assignVar(char* name, number value);
  void assignVar(char* name, int value);
  void assignVar(char* name, float value);
  void assignVar(char* name, char* value);
 
  void dimArrayVar(char* name, int length);
 
- void assignArray(char* name, int index, num value);
+ void assignArray(char* name, int index, number value);
  void assignArray(char* name, int index, int value);
  void assignArray(char* name, int index, float value);
  void assignArray(char* name, int index, char* value);
  
- num getInt(char* name);
+ number getInt(char* name);
  float getFloat(char* name);
  char* getString(char* name);
 
  int getArrayLength(char* name);
 
- num getIntFromArray(char* name, int index);
+ number getIntFromArray(char* name, int index);
  float getFloatFromArray(char* name, int index);
  char* getStringFromArray(char* name, int index);
 
